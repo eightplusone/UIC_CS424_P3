d52e6ff1cf9264c21bc3c46b1eeff9a7b51b0590
Hai Tran <hai.t@outlook.com>
2016-11-29 16:08:01 -0600

    A hidden div in index.html to pass values between js files

--- script/textviz.js
+++ script/textviz.js
@@ -2,14 +2,18 @@ $(document).ready(function(){
 
   // Canvas
   let svg_left = d3.select("body").select("div.textViz").append("svg")
-      .attr("width", "200%")
-      .attr("height", "1000%")
+      .attr("width", "100%")
+      .attr("height", "600%")
       .attr("id", "svg_left");
 
+  let word_position = {};
   let word_distance = 10;
-  let line_height = "60px";
+  let line_height = 50;
   let font_size = "12px";
-  let container_width = $(".textViz").width() - word_distance*2;
+  let line_width = 0;
+  let line_counter = 1;
+
+  let container_width = 320;
   let categories = 0;
    
   // Load data
@@ -26,15 +30,16 @@ $(document).ready(function(){
 
   // Display TextViz
   function display_textviz(content, labels, edges, categories) {
+    // Container
     let doc = svg_left.append("text")
         .attr("x", 0)
         .attr("y", line_height)
         ;
 
-    let line_width = 0;
-    labels.forEach(function(paragraph, i) {
-      paragraph.forEach(function(sentence, i) {
-        sentence.forEach(function(w, i) {
+    // Display the text
+    labels.forEach(function(paragraph, paragraph_idx) {
+      paragraph.forEach(function(sentence, sentence_idx) {
+        sentence.forEach(function(w, word_idx) {
           // Append this word a tspan and measure its width.
           let temp_tspan = svg_left.append("text")
               .text(w.word)
@@ -50,7 +55,14 @@ $(document).ready(function(){
               .text(w.word)
               .style("font-size", font_size)
               ;
+
+            word_position[w.Id] = [
+              line_width + word_distance + Math.round( w_width/2 ), 
+              line_counter * line_height
+            ]
+            
             line_width = line_width + word_distance + w_width;
+
           } else {
             doc.append("tspan")
               .attr("x", word_distance)
@@ -58,21 +70,44 @@ $(document).ready(function(){
               .text(w.word)
               .style("font-size", font_size)
               ;
+
+            line_counter += 1;
+
+            word_position[w.Id] = [
+              word_distance + Math.round( w_width/2 ), 
+              line_counter * line_height
+            ]
+            
             line_width = word_distance + w_width;
           }
           
           temp_tspan.remove();
-          
         })
       })
 
       // Add an extra blank line to separate two paragraphs.
       doc.append("tspan")
-          .attr("x", word_distance)
-          .attr("dy", line_height)
-          .text(" ")
-          .style("font-size", font_size)
-          ;
+        .attr("x", word_distance)
+        .attr("dy", line_height)
+        .text(" ")
+        .style("font-size", font_size)
+        ;
+    })
+    
+    // Draw the edges
+    edges.forEach(function(edge, edge_idx) {
+      if (edge.destinationid.indexOf("T") > -1) {
+        console.log(word_position[ edge.sourceid ][0], word_position[ edge.sourceid ][1]);
+
+        let arrow = "M ";
+        arrow += word_position[ edge.sourceid ][0] + ", " + word_position[ edge.sourceid ][1];
+        arrow += " L ";
+        arrow += word_position[ edge.destinationid ][0] + ", " + word_position[ edge.destinationid ][1];
+
+        svg_left.append("path")
+          .attr("d", arrow)
+          .style("fill", "#000000");
+      }
     })
   }
 });
